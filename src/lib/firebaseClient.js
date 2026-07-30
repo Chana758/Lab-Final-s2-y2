dd3d5bc241ff39357ebcd02da52d82b8798f3e0f
Your Name <your-email@example.com>
2026-07-30 23:46:03 +0700

    Fisrt commmit

--- src/lib/firebaseClient.js
+++ src/lib/firebaseClient.js
@@ -1,8 +1,10 @@
-// src/lib/firebaseClient.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -10,8 +12,15 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Export services សម្រាប់យកទៅប្រើប្រាស់ក្នុង Component ផ្សេងៗ
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+export default app;
