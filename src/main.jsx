25842f33f21b50a27095a5ce93cc037060d23a06
Your Name <your-email@example.com>
2026-07-31 00:00:18 +0700

    Add basename to BrowserRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Lab-Final-s2-y2">
       <App />
     </BrowserRouter>
   </React.StrictMode>
